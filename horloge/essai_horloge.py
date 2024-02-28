95da5a582096b0c9db7e4a40e2dfa2e602ecc736
cassandradelplanque <cassandra25122002@icloud.com>
2024-02-28 17:58:06 +0100

    semaine2 code horloge

--- horloge/essai_horloge.py
+++ horloge/essai_horloge.py
@@ -1,51 +1,60 @@
 import math
-from datetime import datetime
 
 import pygame
 
 
 class Aiguille():
-    def __init__(self):
-        self.theta=0
-        self.theta_int=0
-        self.pas=1
+    def __init__(self,taille=80):
+        self.goal_theta=0 #but
+        self.current_theta=0 #position actuelle
+        self.pas=1 #distance parcourue en une "visualisation"
+        self.taille=taille #taille de l'aiguille
 
-    def set_theta(self,theta):
-        self.theta=theta
+    def set_goal_theta(self,theta):
+        self.goal_theta=theta%360 
 
-    def set_theta_int(self):
-        self.theta_int+=self.pas
+    def set_current_theta(self):
+        self.current_theta+=self.pas
+        self.current_theta=self.current_theta%360
 
     def dessiner_aiguille(self, pos, screen):
-        pos1=(pos[0]+80*math.cos(2*math.pi/360*self.theta_int),pos[1]+ 80*math.sin(2*math.pi/360*self.theta_int))
+        eps=self.goal_theta-self.current_theta #pour tester notre condition d'arrêt
+        next_pas=self.pas # pour que l'arrêt soit fluide 
+        if abs(eps)>=abs(self.pas):
+            self.set_current_theta() #l'aiguille avance
+        elif abs(eps)!=0:
+            next_pas=self.pas #pour que l'aiguile s'arrête exactement a la bonne position
+            self.pas=eps
+            self.set_current_theta()
+        pos1=(pos[0]+self.taille*math.cos(2*math.pi/360*self.current_theta),pos[1]+ self.taille*math.sin(2*math.pi/360*self.current_theta))
         pygame.draw.line(screen, "red", pos, pos1, 5 )
-        if self.theta!=self.theta_int:
-            self.set_theta_int()
+        self.pas=next_pas
+
 
     def set_pas(self,pas):
         self.pas=pas
 
 class Horloge():
-    def __init__(self, pos_0=(0,0)):
+    def __init__(self, pos_0=(0,0), rayon=100, width=3):
         self.pos_0=pos_0
-        self.aiguille_1=Aiguille()
-        self.aiguille_2=Aiguille()
+        self.aiguille_1=Aiguille(rayon*0.8)
+        self.aiguille_2=Aiguille(rayon*0.8)
+        self.rayon=rayon
+        self.width=width
 
     def set_aiguille(self,theta_1,theta_2):
-        self.aiguille_1.set_theta(theta_1)
-        self.aiguille_2.set_theta(theta_2)
+        self.aiguille_1.set_goal_theta(theta_1)
+        self.aiguille_2.set_goal_theta(theta_2)
 
     def set_aig_pas(self,pas1,pas2):
         self.aiguille_1.set_pas(pas1)
         self.aiguille_2.set_pas(pas2)
 
 
-    def dessiner(self, screen,rayon=100,width=5):
-        pygame.draw.circle(screen, "black", self.pos_0, rayon, width)
+    def dessiner(self, screen):
+        pygame.draw.circle(screen, "black", self.pos_0, self.rayon,self.width)
         self.aiguille_1.dessiner_aiguille(self.pos_0,screen)
         self.aiguille_2.dessiner_aiguille(self.pos_0,screen)
-
-
 
 
 
--- horloge/horloge.py
+++ horloge/horloge.py
@@ -1,39 +1,60 @@
 import math
+import sys
 from datetime import datetime
 
+import numpy as np
 import pygame
-
 from essai_horloge import Horloge
 
 # pygame setup
+
+hauteur=int (sys.argv[1])
+largeur=int (sys.argv[2])
+nbligne=int(sys.argv[3])
+nbcolonne=int(sys.argv[4])
+
 pygame.init()
-screen = pygame.display.set_mode((1280, 720))
+screen = pygame.display.set_mode((largeur, hauteur))
 clock = pygame.time.Clock()
 running = True
 dt = 0
 i=0
 j=0
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
-player_pos2= (player_pos[0], player_pos[1]-80)
-horloge=Horloge(player_pos)
-horloge.set_aiguille(45,90)
-horloge.set_aig_pas(1,0.5)
+a=(largeur-20)/ nbcolonne
+b=(hauteur-20) / nbligne
+ray=min(a,b)*0.45
+Liste_horloge=[]
+for i in range(nbcolonne):
+    Liste_horloge2=[]
+    for j in range(nbligne):
+        horloge=Horloge((i*a + a/2 +10,j*b + b/2 +10), ray)
+        horloge.set_aiguille(45,90)
+        horloge.set_aig_pas(1,0.5)
+        Liste_horloge2.append(horloge)
+    Liste_horloge.append(Liste_horloge2)
 
-
+t=0
 while running:
-    i=i+0.5
-    j=j+1
+    now=datetime.now().time()
+
+    t=t+1
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
-
+    
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
-    
-    horloge.dessiner(screen)
-    print(player_pos)
+    for i in range(nbcolonne):
+        for j in range(nbligne):
+            horloge=Liste_horloge[i][j]
+            horloge.dessiner(screen)
+            if(t%500==0):
+                horloge.set_aiguille(horloge.aiguille_1.goal_theta-35,horloge.aiguille_2.goal_theta+45)
+                horloge.aiguille_1.set_pas(horloge.aiguille_1.pas*-1)
+            
+    #print(player_pos)
 
 
 
