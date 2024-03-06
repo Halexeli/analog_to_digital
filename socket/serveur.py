b5befbcc517a73f5cda7e9941d8a6d8a3951af71
cassandradelplanque <cassandra25122002@icloud.com>
2024-03-06 16:19:54 +0100

    correction socket

--- socket/serveur.py
+++ socket/serveur.py
@@ -1,5 +1,6 @@
 import pickle
 import socket
+import time
 
 HOST = "127.0.0.1"  # Standard loopback interface address (localhost)
 PORT = 65432  # Port to listen on (non-privileged ports are > 1023)
@@ -16,9 +17,12 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 if data:
                     # Si des données ont été reçues, les traiter
                     print("Données reçues :", pickle.loads(data))
+                    conn.sendall(data)
                 else:
                     # Si aucune donnée n'a été reçue
                     print("Aucune donnée disponible pour le moment")
             except:
                 # Gérer l'erreur de non-blocage
                 pass
+            print("test")
+            time.sleep(4)
